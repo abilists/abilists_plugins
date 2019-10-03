8728701aff36c712f9c228800f1a9a583196c783
kim joon <minziappa@gmail.com>
2019-10-04 07:44:58 +0900

    Updated survey-0.0.1.sfn

--- src/main/java/com/abilists/plugins/service/PluginService.java
+++ src/main/java/com/abilists/plugins/service/PluginService.java
@@ -1,7 +1,5 @@
 package com.abilists.plugins.service;
 
 public interface PluginService {
-
-	public String createTables() throws Exception;
-
+	public String createTables(String tableName) throws Exception;
 }
