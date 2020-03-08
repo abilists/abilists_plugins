e09416837f88e6251f8fd9808004ab3f44c6d062
kim joon <minziappa@gmail.com>
2020-03-08 22:22:21 +0900

    Fixed bugs to install Plugins

--- src/main/java/com/abilists/plugins/service/PluginService.java
+++ src/main/java/com/abilists/plugins/service/PluginService.java
@@ -1,6 +1,14 @@
 package com.abilists.plugins.service;
 
+import java.util.HashMap;
+
 public interface PluginService {
-	public String createTables(String tableName) throws Exception;
+
+	public static String TABLE_NAME = "mpName";
+	public static String DB_URL = "dbUrl";
+	public static String DB_USERNAME = "dbUsername";
+	public static String DB_PASSWORD = "dbPassword";
+
+	public String createTables(HashMap<String, String> mapHash) throws Exception;
 	public String dropTables(String tableName) throws Exception;
 }
